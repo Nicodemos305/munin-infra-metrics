845f157f08b1d385d6e8e5896e7febc0655c36b1
Felipe Nicodemos <nicodemos@MacBook-Pro-de-Felipe.local>
2021-06-20 16:16:38 -0300

    Implements metric cpu , memory used, available, percent

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,28 @@
-import os, psutil
+import os, psutil, datetime
 from app import app
 
 @app.route('/')
 @app.route('/metrics')
 def index():
-    memmory = get_mommory()
-    metrics = {"memmory": memmory}
+    memmory = get_memory()
+    cpu = get_cpu()
+    uptime = get_boot_time()
+    metrics = {"memory": memmory, "cpu" : cpu, "uptime" : uptime} 
     return metrics
     
-def get_mommory():
-    process = psutil.Process(os.getpid())
-    total_memmory = process.memory_info().rss
-    return total_memmory
+def get_memory():
+    memory_raw = psutil.virtual_memory()
+    memory = { "total" : memory_raw[0], "available" : memory_raw[1],"used" : memory_raw[3], "used_percent" : memory_raw[2]}
+
+    return memory
+
+def get_cpu():
+    cpu_raw = psutil.cpu_count()
+    cpu = {"total_cores" : cpu_raw}
+    return cpu
+
+def get_boot_time():
+    boot_time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    uptime = {"uptime" : today, "boot_time" : boot_time}
+    return uptime
